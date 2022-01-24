10d89e5efdead0243adc9f478504f5604e628386
sarabaruni <s.barooni46@gmail.com>
2022-01-24 13:35:39 +0330

    adding code of counting frequency

---
+++ frequency.py
@@ -0,0 +1,29 @@
+import pickle
+from parsivar import FindStems
+
+
+my_stemmer = FindStems()
+
+
+a_file = open("dataset/frequency_log_set.pkl", "rb")
+
+freq = pickle.load(a_file)
+def frequency(word):
+    try:
+        try:
+            return freq[word]
+        except:
+            return freq[my_stemmer.convert_to_stem(word)]
+    except:
+        return 1
+
+
+if __name__ == "__main__":
+    word = input('please enter your word: ')
+    # your_word = 'کتاب'
+    try:
+        print(frequency(word))
+    except:
+        print("this is a number or is'nt appropriate word")
+
+
